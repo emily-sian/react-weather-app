647a1e30590ee46f52a5382cf2c5d285c04fd8d1
emily-sian <97926966+emily-sian@users.noreply.github.com>
2023-02-12 17:57:25 -0500

    updates github url

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,9 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://github.com/emily-sian/weather-app">Open-Source Code</a>
+      <a href="https://github.com/emily-sian/react-weather-app">
+        Open-Source Code
+      </a>
       by Emily
     </div>
   );
